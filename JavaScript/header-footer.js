6e7af8d4bc5a4d4d78bb71698b9b310d4919917b
Mayron28br <mayronwilke28@gmail.com>
2023-11-28 02:52:33 -0300

    index imagems

--- JavaScript/header-footer.js
+++ JavaScript/header-footer.js
@@ -1,22 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('.navigation a');
-
-    links.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-
-            links.forEach(l => l.classList.remove('selected'));
-            this.classList.add('selected');
-
-            const targetHref = this.getAttribute('href');
-            history.pushState(null, null, targetHref);
-        });
-    });
-});
-
 document.addEventListener("DOMContentLoaded", function () {
     const burgerCheckbox = document.getElementById("burger");
     const navUl = document.querySelector(".navigation ul");
+
+    navUl.style.left = "-100%";
 
     burgerCheckbox.addEventListener("change", function () {
         if (this.checked) {
